e265b0a49272b05cb39faebed3afb4e59e470d87
Freddy <freddyalexander2.chavez@gmail.com>
2022-05-04 21:50:42 -0500

    mongodb

--- db/config.js
+++ db/config.js
@@ -1,11 +1,32 @@
 const mongoose = require('mongoose');
 
 const dbConnection = async () => {
+  await mongoose
+    .connect(process.env.BD_CNN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      tls: true,
+      tlsCAFile: 'db/ca-certificate.crt',
+    })
+    .catch((error) => {
+      return console.log(error);
+    });
+
+  console.log('Conectado a base de datos');
+};
+
+module.exports = { dbConnection }; /*useCreateIndex: true,
+
+/* 
+const fs = require('fs');
+
+const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.BD_CNN, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      /* useCreateIndex: true, */
+      */
+/*
     });
 
     console.log('Base de datos online');
@@ -13,6 +34,4 @@ const dbConnection = async () => {
     console.log(error);
     throw new Error('Error a la hora de inicializar la DB');
   }
-};
-
-module.exports = { dbConnection };
+}; */
